409d3977197273cb69bc415192bb9f427227253e
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-09-22 19:35:39 +0100

    add functionality and passing tests for 06_is_valid_sort_code

--- 06_is_valid_sort_code.py
+++ 06_is_valid_sort_code.py
@@ -16,7 +16,11 @@ def is_valid_sort_code(text):
 
     You should return True if the sort code is valid, and False otherwise
     """
-    pass
+    regex = re.compile(r"^[0-9]{2}-[0-9]{2}-[0-9]{2}$")
+    if regex.search(text):
+        return True
+    else:
+        return False
 
 
 @run_test
@@ -29,7 +33,7 @@ def test_is_valid_sort_code_returns_true_if_sort_code_in_the_correct_format():
         format_err_msg(True, is_valid_sort_code("85-16-23"))
 
 
-@skip_test
+@run_test
 def test_is_valid_sort_code_returns_false_if_sort_code_NOT_in_the_correct_format():
     assert not is_valid_sort_code("51-349-67"), \
         format_err_msg(False, is_valid_sort_code("51-349-67"))
